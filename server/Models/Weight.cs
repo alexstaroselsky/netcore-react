1b00878bf68e3a5d6c3cd308a36d98292f573a2f
Alex Staroselsky <34102969+alexstaroselsky@users.noreply.github.com>
2018-12-25 13:04:16 -0700

    Created weight model.

---
+++ server/Models/Weight.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace server.Models
+{
+    public class Weight
+    {
+        [Key]
+        public string WeightId { get; set; }
+        [Required]
+        public string NDBNumber { get; set; }
+        [Required]
+        public int SequenceNumber { get; set; }
+        [Required]
+        public double Amount { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public double Grams { get; set; }
+        public int NumberDataPoints { get; set; }
+        public double StandardDeviation { get; set; }
+    }
+}
