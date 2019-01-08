ce228f6b97577972e55b184a4b840c7a392133f2
Alex Staroselsky <34102969+alexstaroselsky@users.noreply.github.com>
2019-01-07 20:33:50 -0700

    Added nutrient definition model.

---
+++ server/Models/NutrientDefinition.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace server.Models
+{
+    public class NutrientDefinition
+    {
+        [Key]
+        public string NutrientDefinitionId { get; set; } // Nutr_No
+        [Required]
+        public string Units { get; set; } // Units
+        public string Tag { get; set; } // Tagname
+        [Required]
+        public string Name { get; set; } // NutrDesc
+        [Required]
+        public int Decimals { get; set; } // Num_Dec
+        [Required]
+        public string SortOrder { get; set; }
+    }
+}
