eb074b2c2861ca933cebcc93da6f640a4591fa9e
Alex Staroselsky <34102969+alexstaroselsky@users.noreply.github.com>
2019-01-07 20:37:28 -0700

    Added food group model.

---
+++ server/Models/FoodGroup.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace server.Models
+{
+    public class FoodGroup
+    {
+        [Key]
+        public string FoodGroupId { get; set; } // FdGrp_Cd
+        [Required]
+        public string Name { get; set; }
+    }
+}
