0705f1de1f45bd5412b091c74710ae9acef56f54
Alex Staroselsky <34102969+alexstaroselsky@users.noreply.github.com>
2019-01-08 20:42:04 -0700

    Added food groups controller.

---
+++ server-tests/Controllers/FoodGroupsControllerTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using server.Controllers;
+using server.Models;
+using Xunit;
+
+namespace server_tests.Controllers
+{
+    public class FoodGroupsControllerTests
+    {
+        [Fact]
+        public void ReturnsFoodGroups()
+        {
+            // Arrange
+            var controller = new FoodGroupsController();
+
+            // Act
+            var result = controller.Get();
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var returnValue = Assert.IsType<List<FoodGroup>>(okResult.Value);
+        }
+    }
+}
---
+++ server/Controllers/FoodGroupsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using server.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace server.Controllers
+{
+    [Route("api/[controller]")]
+    public class FoodGroupsController : Controller
+    {
+        // GET: api/foodgroups
+        [HttpGet]
+        public ActionResult Get()
+        {
+            var foodGroups = new List<FoodGroup>();
+            return Ok(foodGroups);
+        }
+    }
+}
