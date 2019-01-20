f9df0b32bb7b5b83e5fc8da776efd6c737d1b875
Alex Staroselsky <34102969+alexstaroselsky@users.noreply.github.com>
2019-01-19 18:09:39 -0700

    Created class for data seeding.

---
+++ src/Data/SeedData.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using NutritionaData.Backend.Models;
+
+namespace NutritionData.Backend.Data
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new NutritionDataContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<NutritionDataContext>>()))
+            {
+                if (context.Foods.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                List<Weight> weights = GetWeights();
+            }
+        }
+
+        /// <summary>
+        /// Gets weights from weights CSV/TXT file.
+        /// </summary>
+        /// <returns>List of weights</returns>
+        public static List<Weight> GetWeights()
+        {
+            return System.IO.File.ReadAllLines(@"Data/WEIGHT.txt")
+                .Select(row => row.Split('^'))
+                .Select(columns => new Weight
+                {
+                    NDBNumber = columns[0].Replace("~", ""),
+                    SequenceNumber = columns[1].Replace("~", ""),
+                    Amount = Double.TryParse(columns[2], out double amount) ? amount : -1,
+                    Description = columns[3].Replace("~", ""),
+                    Grams = Double.TryParse(columns[4], out double grams) ? grams : -1,
+                    NumberDataPoints = Int32.TryParse(columns[5], out int numberDataPoints) ? numberDataPoints : -1,
+                    StandardDeviation = Double.TryParse(columns[6], out double standardDeviation) ? standardDeviation : -1
+
+                })
+                .ToList();
+        }
+    }
+}
